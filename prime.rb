1c2637bf3f2834d7eacdf97364ba8f24e3cf8a87
Mehmet <mehmetkusken@gmail.com>
2021-02-09 21:32:54 -0700

    commit tree 57213daa881b5c7b5602c1f9cce6b97791fc0f18

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(number)
+    if number <=1
+        return false
+    elsif number ==2
+        return true
+    end
+    (2..number-1).each do |n|
+        if number%n == 0
+            return false
+        end
+        end
+        return true
+    
+end
